cc1b8cc48254405ae9e2bdf9b8390a80c749b76e
lPat1ence01 <marcochb0102@outlook.com>
2025-05-08 17:16:15 -0500

    Agregar archivos de proyecto.

---
+++ EMSI_Corporation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMSI_Corporation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
