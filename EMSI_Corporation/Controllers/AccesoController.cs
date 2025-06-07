90aa098c816ba3b69aff141fc457bfb5b13834a7
kev-developer <kevpersonal3@.com>
2025-06-07 15:16:43 -0500

    Agregada librería de EPPlusSoftware para excel. Agregado View intermedio para mantenimiento

--- EMSI_Corporation/Controllers/AccesoController.cs
+++ EMSI_Corporation/Controllers/AccesoController.cs
@@ -4,6 +4,9 @@ using EMSI_Corporation.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Headers;
+using OfficeOpenXml;
 
 namespace EMSI_Corporation.Controllers
 {
@@ -27,5 +30,71 @@ namespace EMSI_Corporation.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult PopupMant()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GoToMant(ClienteTrabajadorVM cliente_trabajador)
+        {
+            /*using var workbook = new XLWorkbook();
+            var worksheet = workbook.AddWorksheet("Sample Sheet");
+            worksheet.Cell("A1").Value = "Hello World 2!";
+            worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
+            workbook.Save();*/
+            
+            return View("Mantenimiento",cliente_trabajador);
+        }
+
+        [HttpGet]
+        public FileResult DescargarExcel()
+        {
+            ExcelPackage.License.SetNonCommercialPersonal("EMSI");
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Datos");
+
+                worksheet.Row(1).Style.Fill.SetBackground(System.Drawing.Color.AliceBlue);
+                worksheet.Cells[1, 1, 1, 5].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin, System.Drawing.Color.Black, true);
+                worksheet.Row(1).CustomHeight = true;
+                worksheet.Row(1).Height = 50;
+
+                // Agrega datos
+                worksheet.Cells[1, 1].Value = "Nombre";
+                worksheet.Cells[1, 2].Value = "Edad";
+
+                worksheet.Cells[2, 1].Value = "Julissa";
+                worksheet.Cells[2, 2].Value = 25;
+
+                var stream = new MemoryStream(package.GetAsByteArray());
+
+                return File(stream.ToArray(),
+                            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                            "reporte.xlsx");
+            }
+        }
+
+        /*
+         //Este funciona pero descarga solo a nivel de backend, es decir, no lo descarga en la computadora del frontend, se guarda el excel dentro de la solución
+        [HttpGet]
+        public void DownloadFile()
+        {
+            using var workbook = new XLWorkbook();
+            var worksheet = workbook.AddWorksheet("Sample Sheet");
+            worksheet.Cell("A1").Value = "Hello World!";
+            worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
+            workbook.SaveAs("HelloWorld.xlsx");
+            
+        }*/
+
+        
+        /*
+        [HttpPost]
+        public IActionResult UploadNewProd()//falta VM
+        {
+            //return View("Mantenimiento");
+        }*/
     }
 }
---
+++ EMSI_Corporation/ViewModels/ClienteTrabajadorVM.cs
@@ -0,0 +1,8 @@
+﻿namespace EMSI_Corporation.ViewModels
+{
+    public class ClienteTrabajadorVM
+    {
+        public string nom_cliente { get; set; }
+        public string nom_trabajador { get; set; }
+    }
+}
